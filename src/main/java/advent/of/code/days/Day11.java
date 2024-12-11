1b66157da5f30e19e03a6e7a90033e9892b35044
Daniel Avery <danielaveryj@gmail.com>
2024-12-11 13:50:16 -0700

    Simplify day 11.

--- src/main/java/advent/of/code/days/Day11.java
+++ src/main/java/advent/of/code/days/Day11.java
@@ -18,44 +18,22 @@ public class Day11 implements Day {
     }
     
     void solve(IO io, int blinks) {
-        // First discover all the progeny, so that we can apply bottom-up dynamic programming.
-        List<Long> initialStones = io.scanner().tokens().map(Long::parseLong).toList(), curr = new ArrayList<>(initialStones);
-        Map<Long, List<Long>> childrenByParent = new HashMap<>();
+        Map<Long, Long> curr = io.scanner().tokens().map(Long::parseLong).collect(Collectors.groupingBy(i -> i, Collectors.counting()));
         for (int i = 0; i < blinks; i++) {
-            List<Long> next = new ArrayList<>();
-            for (long stone : curr) {
-                if (childrenByParent.getOrDefault(stone, List.of()).isEmpty()) {
-                    List<Long> children = blink(stone);
-                    childrenByParent.put(stone, children);
-                    for (long child : children) {
-                        if (childrenByParent.putIfAbsent(child, List.of()) == null) {
-                            next.add(child);
-                        }
-                    }
+            Map<Long, Long> next = new HashMap<>();
+            curr.forEach((stone, count) -> {
+                String s;
+                if (stone == 0) {
+                    next.merge(1L, count, Long::sum);
+                } else if ((s = Long.toString(stone)).length() % 2 == 0) {
+                    next.merge(Long.parseLong(s.substring(0, s.length()/2)), count, Long::sum);
+                    next.merge(Long.parseLong(s.substring(s.length()/2)), count, Long::sum);
+                } else {
+                    next.merge(stone * 2024, count, Long::sum);
                 }
-            }
+            });
             curr = next;
         }
-        // Good to go.
-        Map<Long, long[]> memos = childrenByParent.keySet().stream().collect(Collectors.toMap(s -> s, _ -> new long[]{ 1, 0 }));
-        for (int i = 0; i < blinks; i++) {
-            childrenByParent.forEach((parent, children) ->
-                memos.get(parent)[1] = children.stream().mapToLong(child -> memos.get(child)[0]).sum()
-            );
-            memos.forEach((_, memo) -> memo[0] = memo[1]);
-        }
-        long sum = initialStones.stream().mapToLong(s -> memos.get(s)[0]).sum();
-        io.out().println(sum);
-    }
-    
-    List<Long> blink(long stone) {
-        if (stone == 0) {
-            return List.of(1L);
-        }
-        String s = Long.toString(stone);
-        if (s.length() % 2 == 0) {
-            return List.of(Long.parseLong(s.substring(0, s.length()/2)), Long.parseLong(s.substring(s.length()/2)));
-        }
-        return List.of(stone * 2024);
+        io.out().println(curr.values().stream().mapToLong(i -> i).sum());
     }
 }
